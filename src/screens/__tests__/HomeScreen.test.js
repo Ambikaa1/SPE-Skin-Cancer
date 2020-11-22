7d3fb3f0cd82a12cf4c46e87243e0db7a39d79e5
George Herbert <george.herbert@icloud.com>
2020-11-22 18:53:36 +0000

    Fixed tests

--- src/screens/__tests__/HomeScreen.test.js
+++ src/screens/__tests__/HomeScreen.test.js
@@ -1,16 +1,16 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
 
-import App from '../App';
+import HomeScreen from "../HomeScreen";
 
-describe('<App />', () => {
+describe('<HomeScreen />', () => {
   it('has 1 child', () => {
-    const tree = renderer.create(<App />).toJSON();
+    const tree = renderer.create(<HomeScreen />).toJSON();
     expect(tree.children.length).toBe(1);
   });
 
   it('renders correctly', () => {
-    const tree = renderer.create(<App />).toJSON();
+    const tree = renderer.create(<HomeScreen />).toJSON();
     expect(tree).toMatchSnapshot();
   });
 });
