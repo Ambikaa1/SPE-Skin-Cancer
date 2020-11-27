88645b71abd1b3923f5b5834a32b6a632c25ca6c
George Herbert <george.herbert@icloud.com>
2020-11-27 01:22:39 +0000

    Nested a stack navigator in the InfoScreen.js so information can be recursively looked through

--- App.js
+++ App.js
@@ -2,21 +2,18 @@ import React from "react"
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import HomeScreen from "./src/screens/HomeScreen";
-import InfoScreen from "./src/screens/InfoScreen";
+import InfoStack from "./src/screens/InfoScreen";
 import CameraScreen from "./src/screens/CameraScreen";
 import DiaryScreen from "./src/screens/DiaryScreen";
 import SendScreen from "./src/screens/SendScreen";
-
-// This is temporary
-const data = [{title: "What is skin cancer?", children: [{title: "The common types of skin cancer", children: [{title: "Basal cell carcinoma"}, {title: "Squamous cell carcinoma"}, {title: "Malignant melanoma"}]}, {title: "How skin cancers are triggered"}, {title: "What skin cancer looks like"}, {title: "Treatments for skin cancer"}]},{title: "How do I monitor my moles?",},{title: "What is the charity SCaRF?",},{title: "How do I donate?",}]
 
 const Tab = createBottomTabNavigator();
 
 const MyTabs = () => {
   return (
-    <Tab.Navigator data = {data}>
+    <Tab.Navigator>
       <Tab.Screen name = "Home" component = {HomeScreen} />
-      <Tab.Screen name = "Info" children = {props => <InfoScreen {...props} data = {data} />} />
+      <Tab.Screen name = "Info" component = {InfoStack} />
       <Tab.Screen name = "Camera" component = {CameraScreen} />
       <Tab.Screen name = "Diary" component = {DiaryScreen} />
       <Tab.Screen name = "Send" component = {SendScreen} />
--- src/screens/InfoScreen.js
+++ src/screens/InfoScreen.js
@@ -1,9 +1,23 @@
 import React, { useState } from "react";
 import { SafeAreaView, FlatList, StyleSheet, TouchableOpacity } from "react-native";
+import { createStackNavigator } from '@react-navigation/stack';
 import SearchBar from "../components/SearchBar";
 import InfoItem from "../components/InfoItem";
 
-const InfoScreen = ({ navigation, data }) => {
+// This is temporary
+const data = [{title: "What is skin cancer?", children: [{title: "The common types of skin cancer", children: [{title: "Basal cell carcinoma"}, {title: "Squamous cell carcinoma"}, {title: "Malignant melanoma"}]}, {title: "How skin cancers are triggered"}, {title: "What skin cancer looks like"}, {title: "Treatments for skin cancer"}]},{title: "How do I monitor my moles?",},{title: "What is the charity SCaRF?",},{title: "How do I donate?",}]
+
+const Stack = createStackNavigator()
+
+const InfoStack = () => {
+  return(
+    <Stack.Navigator>
+      <Stack.Screen name = "Info" component = {InfoScreen} />
+    </Stack.Navigator>
+  );
+};
+
+const InfoScreen = ({ navigation }) => {
   const [term, setTerm] = useState("")
   return (
     <SafeAreaView style = {styles.container}>
@@ -17,7 +31,10 @@ const InfoScreen = ({ navigation, data }) => {
         keyExtractor = {({ title }) => title}
         renderItem = {({ item }) => {
           return (
-            <TouchableOpacity>
+            <TouchableOpacity onPress = {() => {
+              navigation.push("Info")
+            }
+            }>
               <InfoItem name = {item.title}/>
             </TouchableOpacity>
           );
@@ -33,4 +50,4 @@ const styles = StyleSheet.create({
   },
 });
 
-export default InfoScreen;
+export default InfoStack;
