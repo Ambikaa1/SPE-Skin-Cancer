7db492b431668c0749c0b9d69e0f26edc05d1709
George Herbert <george.herbert@icloud.com>
2021-02-05 17:33:42 +0000

    Can now add a named mole to a body part in sqlite

--- src/screens/StorageScreen.js
+++ src/screens/StorageScreen.js
@@ -2,21 +2,34 @@ import React, { useEffect } from "react";
 import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
 import * as SQLite from "expo-sqlite";
 
-const db = SQLite.openDatabase("db.db")
+const db = SQLite.openDatabase("f.db")
+db.exec([{ sql: 'PRAGMA foreign_keys = ON;', args: [] }], false, () =>
+  console.log('Foreign keys turned on')
+);
+// db.exec([{ sql: 'drop table if exists mole;', args: [] }], false, () => {});
 
 const StorageScreen = ({ route }) => {
 
   useEffect(() => {
 
     db.transaction(tx => {
-      // tx.executeSql("drop table if exists mole;")
-      tx.executeSql("create table if not exists hum (name text primary key not null unique);", []);
-      tx.executeSql("create table if not exists mole (id integer primary key not null, name text, part text, foreign key(part) references hum(name));", []);
+      tx.executeSql(
+        "create table if not exists humonculus (name text primary key not null unique);",
+        [],
+        null,
+        (t, error) => {console.log(error);}
+      );
+      tx.executeSql(
+        "create table if not exists mole (id integer primary key not null unique, name text, body_part text references humonculus(name));",
+        [],
+        null,
+        (t, error) => {console.log(error);}
+      );
       tx.executeSql("create table if not exists test (id integer primary key not null, name text);", []); // temporary
-      tx.executeSql("insert into hum (name) values ('head');", []);
-      tx.executeSql("insert into hum (name) values ('body');", []);
-      tx.executeSql("insert into hum (name) values ('arms');", []);
-      tx.executeSql("insert into hum (name) values ('legs');", []);
+      tx.executeSql("insert into humonculus (name) values ('head');", []);
+      tx.executeSql("insert into humonculus (name) values ('body');", []);
+      tx.executeSql("insert into humonculus (name) values ('arms');", []);
+      tx.executeSql("insert into humonculus (name) values ('legs');", []);
     });
   });
 
@@ -24,42 +37,59 @@ const StorageScreen = ({ route }) => {
     <View>
       <TouchableOpacity onPress = {() => db.transaction(
         tx => {
-          tx.executeSql("insert into test (name) values ('test2');", []);
-          tx.executeSql("select * from test;", [], (_, { rows }) =>
-            console.log(rows)
+          tx.executeSql(
+            "insert into test (name) values ('test2');",
+            [],
+            null,
+            (t, error) => {console.log(error);}
           );
-        })}
-      >
-        <Text style = {styles.text}>ADD TEST</Text>
+        }
+      )}>
+        <Text style = {styles.text}>ADD_TEST</Text>
       </TouchableOpacity>
       <TouchableOpacity onPress = {() => db.transaction(
         tx => {
-          tx.executeSql("select * from hum;", [], (_, { rows }) =>
-            console.log(rows)
+          tx.executeSql(
+            "insert into mole (name, body_part) values ('Mole', 'head');",
+            [],
+            null,
+            (t, error) => {console.log(error);}
           );
-        })}
-      >
-        <Text style = {styles.text}>VIEW_BODY</Text>
+        }
+      )}>
+        <Text style = {styles.text}>ADD_MOLE</Text>
+      </TouchableOpacity>
+
+      <TouchableOpacity onPress = {() => db.transaction(
+        tx => {
+          tx.executeSql(
+            "select * from test;",
+            [],
+            (_, { rows }) => {console.log(rows)},
+            (t, error) => {console.log(error);})
+        }
+      )}>
+        <Text style = {styles.text}>VIEW_TEST</Text>
       </TouchableOpacity>
       <TouchableOpacity onPress = {() => db.transaction(
         tx => {
-          tx.executeSql("insert into mole (name) values ('head mole');", []);
           tx.executeSql("select * from mole;", [], (_, { rows }) =>
             console.log(rows)
           );
         })}
       >
-        <Text style = {styles.text}>HEAD</Text>
+        <Text style = {styles.text}>VIEW_MOLE</Text>
       </TouchableOpacity>
       <TouchableOpacity onPress = {() => db.transaction(
         tx => {
-          tx.executeSql("insert into mole (name, part) values ('body mole', 'body');", []);
-          tx.executeSql("select * from mole;", [], (_, { rows }) =>
-            console.log(rows)
-          );
-        })}
-      >
-        <Text style = {styles.text}>BODY</Text>
+          tx.executeSql(
+            "select * from humon;",
+            [],
+            (_, { rows }) => {console.log(rows)},
+            (t, error) => {console.log(error);})
+        }
+      )}>
+        <Text style = {styles.text}>VIEW_</Text>
       </TouchableOpacity>
     </View>
   );
