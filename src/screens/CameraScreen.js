953cc54f940e8430580413013e337e70c1bf7382
EmilyLB <bu19067@bristol.ac.uk>
2021-02-05 16:35:37 +0000

    Added expo camera stuff

--- src/screens/CameraScreen.js
+++ src/screens/CameraScreen.js
@@ -2,67 +2,50 @@ import React, {useEffect, useState} from "react";
 import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
 import { Camera } from "expo-camera";
 
+
+
 // import {useAsyncStorage} from "RNAsyncStorage";
 // const handlePress = () =>console.log("Test pressed")
 
 const CameraScreen = () => {
-    const [hasPermission, setHasPermission] = useState(null); //Initially we have no state
+    //Set initial camera permissions as nul.
+    const [hasPermission, setHasPermission] = useState(null);
+    const [type, setType] = useState(Camera.Constants.Type.back);
 
-    return(
-    <View style = {styles.container}>
-        { hasPermission
-            ? (<StartCamera />)
-            : (
-                <View style = {styles.container}>
-                    <Text style = {styles.message}>
-                        Here you can take photos of your moles!
-                    </Text>
-                    <TouchableOpacity
-                    onPress={useEffect(() => { //Use effect allows us to do work after we render, in this case getting permissions
-                        (async () => {
-                            const { permission } = await Camera.requestPermissionsAsync();
-                            setHasPermission(permission === 'granted');
-                        })()},[])}
-                    style={{width: 200, borderRadius: 4, backgroundColor: "darkblue", flexDirection: 'row', justifyContent: 'center', alignItems: 'center', height: 50}}
-                    >
-                        <Text style={{color: '#fff', fontWeight: 'bold', textAlign: 'center'}}>
-                            Get Started!
-                        </Text>
-                    </TouchableOpacity>
-                </View>
-            )
-        }
-    </View>
-    );
-};
+    useEffect(() => {
+        (async () => {
+            const { status } = await Camera.requestPermissionsAsync();
+            setHasPermission(status === 'granted');
+        })();
+    }, []);
 
-const StartCamera = () => {
-    const [cameraFacing , setCameraFacing] = useState(Camera.Constants.Type.back); //Set initial camera face to the back camera
-    const [flash, setFlash] = useState('auto') //Initialise flash as auto
-    //const [showingPhoto, setShowingPhoto] = useState('false')
-    //const [photo, setPhoto] = useState<any>null
-
+    if (hasPermission === null) {
+        return <View />;
+    }
+    if (hasPermission === false) {
+        return <Text>No access to camera</Text>;
+    }
     return (
-        <View style = {styles.container}>
-            <Camera
-                type = {cameraFacing}
-                flashMode = {flash}
-                style = {styles.camera}
-                ref = {camera => this.camera = camera}
-            >
-                <View
-                    style={{
-                        flex: 1,
-                        width: '100%',
-                        backgroundColor: 'transparent',
-                        flexDirection: 'row',
-                        justifyContent: 'space-between'
-                    }}>
+        <View style={styles.container}>
+            <Camera style={styles.camera} type={type}>
+                <View style={styles.buttonContainer}>
+                    <TouchableOpacity
+                        style={styles.button}
+                        onPress={() => {
+                            setType(
+                                type === Camera.Constants.Type.back
+                                    ? Camera.Constants.Type.front
+                                    : Camera.Constants.Type.back
+                            );
+                        }}>
+                        <Text style={styles.text}> Flip </Text>
+                    </TouchableOpacity>
                 </View>
             </Camera>
         </View>
     );
 };
+
 
 
 const styles = StyleSheet.create({
@@ -77,7 +60,7 @@ const styles = StyleSheet.create({
         fontWeight: 'bold',
     },
     camera: {
-      flex: 1,
+        flex: 1,
     }
 });
 
