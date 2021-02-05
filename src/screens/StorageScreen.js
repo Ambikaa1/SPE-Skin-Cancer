cf0b84baf5cb1ea2936559dcebf860f8d516107c
George Herbert <george.herbert@icloud.com>
2021-02-05 15:28:08 +0000

    Basic SQLite database now set up

--- src/screens/StorageScreen.js
+++ src/screens/StorageScreen.js
@@ -1,10 +1,29 @@
-import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import React, { useEffect } from "react";
+import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
+import * as SQLite from "expo-sqlite";
+
+const db = SQLite.openDatabase("db.db")
 
 const StorageScreen = ({ route }) => {
+
+  useEffect(() => {
+    db.transaction(tx => {
+      tx.executeSql("create table if not exists body (id integer primary key not null, name text)");
+    });
+  });
+
   return (
     <View>
-      <Text>Test</Text>
+      <TouchableOpacity onPress = {() => db.transaction(
+        tx => {
+          tx.executeSql("insert into body (name) values ('test')");
+          tx.executeSql("select * from body", [], (_, { rows }) =>
+            console.log(JSON.stringify(rows))
+          );
+        })}
+      >
+        <Text>Test</Text>
+      </TouchableOpacity>
     </View>
   );
 };
