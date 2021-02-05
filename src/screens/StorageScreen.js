c5178498c8bc9cb27d4b8449ce6bfff4042640cc
George Herbert <george.herbert@icloud.com>
2021-02-05 15:02:05 +0000

    Added a screen to test storage

---
+++ src/screens/StorageScreen.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { View, Text, StyleSheet } from "react-native";
+
+const StorageScreen = ({ route }) => {
+  return (
+    <View>
+      <Text>Test</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create();
+
+export default StorageScreen;
