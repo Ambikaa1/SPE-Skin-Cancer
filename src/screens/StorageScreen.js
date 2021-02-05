f950ff1f0f289dc8ff6c9c095cff697a30f00cbc
George Herbert <george.herbert@icloud.com>
2021-02-05 16:11:10 +0000

    Made progress with database so multiple tables now

--- src/screens/StorageScreen.js
+++ src/screens/StorageScreen.js
@@ -7,8 +7,16 @@ const db = SQLite.openDatabase("db.db")
 const StorageScreen = ({ route }) => {
 
   useEffect(() => {
+
     db.transaction(tx => {
-      tx.executeSql("create table if not exists body (id integer primary key not null, name text)");
+      // tx.executeSql("drop table if exists mole;")
+      tx.executeSql("create table if not exists hum (name text primary key not null unique);", []);
+      tx.executeSql("create table if not exists mole (id integer primary key not null, name text, part text, foreign key(part) references hum(name));", []);
+      tx.executeSql("create table if not exists test (id integer primary key not null, name text);", []); // temporary
+      tx.executeSql("insert into hum (name) values ('head');", []);
+      tx.executeSql("insert into hum (name) values ('body');", []);
+      tx.executeSql("insert into hum (name) values ('arms');", []);
+      tx.executeSql("insert into hum (name) values ('legs');", []);
     });
   });
 
@@ -16,18 +24,51 @@ const StorageScreen = ({ route }) => {
     <View>
       <TouchableOpacity onPress = {() => db.transaction(
         tx => {
-          tx.executeSql("insert into body (name) values ('test')");
-          tx.executeSql("select * from body", [], (_, { rows }) =>
-            console.log(JSON.stringify(rows))
+          tx.executeSql("insert into test (name) values ('test2');", []);
+          tx.executeSql("select * from test;", [], (_, { rows }) =>
+            console.log(rows)
           );
         })}
       >
-        <Text>Test</Text>
+        <Text style = {styles.text}>ADD TEST</Text>
+      </TouchableOpacity>
+      <TouchableOpacity onPress = {() => db.transaction(
+        tx => {
+          tx.executeSql("select * from hum;", [], (_, { rows }) =>
+            console.log(rows)
+          );
+        })}
+      >
+        <Text style = {styles.text}>VIEW_BODY</Text>
+      </TouchableOpacity>
+      <TouchableOpacity onPress = {() => db.transaction(
+        tx => {
+          tx.executeSql("insert into mole (name) values ('head mole');", []);
+          tx.executeSql("select * from mole;", [], (_, { rows }) =>
+            console.log(rows)
+          );
+        })}
+      >
+        <Text style = {styles.text}>HEAD</Text>
+      </TouchableOpacity>
+      <TouchableOpacity onPress = {() => db.transaction(
+        tx => {
+          tx.executeSql("insert into mole (name, part) values ('body mole', 'body');", []);
+          tx.executeSql("select * from mole;", [], (_, { rows }) =>
+            console.log(rows)
+          );
+        })}
+      >
+        <Text style = {styles.text}>BODY</Text>
       </TouchableOpacity>
     </View>
   );
 };
 
-const styles = StyleSheet.create();
+const styles = StyleSheet.create({
+  text: {
+    fontSize: 50
+  }
+});
 
 export default StorageScreen;
