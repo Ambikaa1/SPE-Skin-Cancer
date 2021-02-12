7f5052f20394ae76915058af61a51d1511349f33
George Herbert <george.herbert@icloud.com>
2021-02-12 17:12:16 +0000

    Fixed dependency issues

--- src/screens/ViewImageScreen.js
+++ src/screens/ViewImageScreen.js
@@ -7,7 +7,7 @@ const ViewImageScreen = () => {
     <View>
       <Image
         style = {styles.Image}
-        source = {{ uri: "file:///var/mobile/Containers/Data/Application/46515454-770C-48CF-9C98-EBF92EE0EB20/Documents/ExponentExperienceData/%2540anonymous%252Fapp-8345b4ac-2bbe-446b-9745-ddcdc28f1dbb/near_shot/test.jpg"}}
+        source = {{ uri: "file:///var/mobile/Containers/Data/Application/B6F3B8DD-AAC7-42FF-B4FA-368369425E9D/Documents/ExponentExperienceData/%2540anonymous%252Fapp-8345b4ac-2bbe-446b-9745-ddcdc28f1dbb/near_shot/test.jpg"}}
       />
     </View>
   );
