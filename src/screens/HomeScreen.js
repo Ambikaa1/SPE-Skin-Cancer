e04b0c61b614ea34940ad686eabf25550eb6145a
George Herbert <george.herbert@icloud.com>
2021-02-12 22:39:57 +0000

    Updated the homee screen to include justgiving logo and working date

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,8 +1,10 @@
-import React from "react";
-import { View, Text, StyleSheet, Linking, TouchableOpacity, Image, Button} from "react-native";
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
-import Homunculous from "./Homunculous";
+import React, { useEffect, useState } from "react";
+import { View, SafeAreaView, Text, StyleSheet, Linking, TouchableOpacity, Image, Dimensions } from "react-native";
+import { StatusBar } from "expo-status-bar";
+
+// import { NavigationContainer } from '@react-navigation/native';
+// import { createStackNavigator } from '@react-navigation/stack';
+// import Homunculous from "./Homunculous";
 
 //import NavigationContainer from "@react-navigation/native/lib/typescript/src/NavigationContainer";
 
@@ -11,55 +13,79 @@ import Homunculous from "./Homunculous";
 // }
 
 
+// {/*<Button*/}
+// {/*    title="Go to homunculous"*/}
+// {/*    onPress={() => navigation.navigate('Homunculous')}*/}
+// {/*/>*/}
+// {/*<NavigationContainer>*/}
+// {/*</NavigationContainer>*/}
 
 const HomeScreen = () => {
+    const [currentDate, setCurrentDate] = useState("");
+
+    useEffect(() => {
+        let daysOfWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+        let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+        let day = daysOfWeek[new Date().getDay()]
+        let date = new Date().getDate();
+        let month = months[new Date().getMonth()];
+        setCurrentDate(day + " " + date + " " + month);
+    }, []);
+
     return (
-        <>
-            {/*<Button*/}
-            {/*    title="Go to homunculous"*/}
-            {/*    onPress={() => navigation.navigate('Homunculous')}*/}
-            {/*/>*/}
-            {/*<NavigationContainer>*/}
-
-            {/*</NavigationContainer>*/}
-
-            <View style = {styles.head}>
-                <Text style = {styles.base}>
-                    <Text style = {styles.titleText}> Welcome Joe! </Text>
-                </Text>
+        <SafeAreaView style = {styles.container}>
+            <View style = {styles.toptext}>
+                <Text style = {styles.date}>{currentDate}</Text>
+                <Text style = {styles.welcome}>Welcome Joe!</Text>
             </View>
-            <View style = {styles.body}>
-                <Text style = {styles.base}>
-                    <TouchableOpacity onPress={() => Linking.openURL('https://www.skincancerresearch.org/what-we-do')}>
-                        <Text style = {{color: 'blue'}}> Donate to SCaRF </Text>
-                    </TouchableOpacity>
-                    <TouchableOpacity>
-                        <Image source = {require('../../assets/SCaRF_Logo.png')} />
-                    </TouchableOpacity>
-                </Text>
+
+            
+            <View style = {styles.logosContainer}>
+                <TouchableOpacity>
+                    <Text style = {styles.textAboveLogo}>About SCaRF</Text>
+                    <Image style = {styles.scarfLogo} source = {require('../../assets/scarf_logo.jpg')} />
+                </TouchableOpacity>
+                <TouchableOpacity>
+                <Text style = {styles.textAboveLogo}>Donate</Text>
+                    <Image style = {styles.scarfLogo} source = {require('../../assets/justgiving_logo.png')} />
+                </TouchableOpacity>
             </View>
-            {/*<View>*/}
-            {/*    <Image source={require('../../assets/SCaRF_Logo.png')}  />*/}
-            {/*</View>*/}
-        </>
+            <StatusBar style = "dark" />
+        </SafeAreaView>
     );
 };
 
 const styles = StyleSheet.create({
-    head: {
-        padding: 35, //instead of this we should find out the size of status bar
-        alignItems: 'center',
+    container: {
+        flex: 1
     },
-    body: {
-        flex: 1,
-        flexDirection: 'column',
-        alignItems: 'center',
-        justifyContent: 'center',
+    date: {
+        fontSize: 30,
+        fontWeight: "bold",
+        marginLeft: 10
     },
-    titleText: {
-        fontSize: 20
+    welcome: {
+        fontSize: 30,
+        fontWeight: "bold",
+        marginLeft: 10
     },
-    base: {
+    logosContainer: {
+        flexDirection: "row",
+        justifyContent: "center",
+        marginHorizontal: 5,
+        bottom: 10,
+        position: "absolute"
+    },
+    textAboveLogo: {
+        marginLeft: 5,
+        fontSize: 25,
+        fontWeight: "bold",
+        alignSelf: "center"
+    },
+    scarfLogo: {
+        width: Dimensions.get("window").width / 2 - 15,
+        height: Dimensions.get("window").width / 2 - 15,
+        marginHorizontal: 5,
     }
 });
 
