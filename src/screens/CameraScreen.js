6bd6a4f1f5b2d117b3e501eeae9bf3c6955cb6d9
EmilyLB <bu19067@bristol.ac.uk>
2021-02-13 11:04:17 +0000

    Basic picture preview working.

--- src/screens/CameraScreen.js
+++ src/screens/CameraScreen.js
@@ -12,6 +12,7 @@ const CameraScreen = () => {
     const [mediaPermission, setMediaPermission] = useState(null);
     const [image, setImage] = useState(null);
     const [ghostImage, setGhostImage] = useState(true);
+    const [photoTaken, setPhotoTaken] = useState(false);
 
     useEffect(() => {
         (async () => {
@@ -33,62 +34,72 @@ const CameraScreen = () => {
     }
     return (
         <View style={styles.container}>
-            <SafeAreaView style={styles.topRow}>
-                <TouchableOpacity onPress={() => Alert.alert('Arrow Pressed')}>
-                    <AntDesign name="arrowleft" size={35} color="black"/>
-                </TouchableOpacity>
-                <TouchableOpacity onPress={() => Alert.alert('Help Pressed')}>
-                    <AntDesign name="questioncircleo" size={35} color="black" />
-                </TouchableOpacity>
-            </SafeAreaView>
-            <Camera style={styles.camera} type={type}
-                    ref={ref => {setCameraRef(ref)}}>
-            </Camera>
-            {ghostImage && <Image
-                style={styles.image}
-                source={require('./cute.jpg')}
-            />}
-            <View style={styles.cameraBar}>
-                <TouchableOpacity
-                    // style={styles.flipCamera}
-                    onPress={() => {
-                        setType(
-                            type === Camera.Constants.Type.back
-                                ? Camera.Constants.Type.front
-                                : Camera.Constants.Type.back
-                        );
-                    }}>
-                    <MaterialCommunityIcons name="rotate-3d-variant" size={50} color="white" />
-                </TouchableOpacity>
+            <ViewTopBar/>
 
-                <TouchableOpacity
-                    // style={styles.takePicture}
-                    onPress={async() => {
-                        if (cameraRef) {
-                            let photo = await cameraRef.takePictureAsync();
-                            // Check users permissions to accessing camera roll
-                            console.log('photo taken', photo);
-                            if (mediaPermission) {
-                                console.log('Access to camera roll')
-                                await MediaLibrary.saveToLibraryAsync(photo.uri)
-                                console.log('Photo saved')
+            <View style={styles.container}>
+
+                {/*If photo has not been taken then show the camera*/}
+                {!photoTaken && <Camera style={styles.camera} type={type}
+                        ref={ref => {setCameraRef(ref)}}>
+                </Camera>}
+
+                {/*If a photo has been taken then show the photo*/}
+                {photoTaken && <Image
+                    style = {styles.camera}
+                    source={{uri : image}}
+                />}
+
+                {ghostImage && <Image
+                    style={styles.image}
+                    source={require('./cute.jpg')}
+                />}
+
+                <View style={styles.cameraBar}>
+                    <TouchableOpacity
+                        onPress={() => {
+                            setType(
+                                type === Camera.Constants.Type.back
+                                    ? Camera.Constants.Type.front
+                                    : Camera.Constants.Type.back
+                            );
+                        }}>
+                        <MaterialCommunityIcons name="rotate-3d-variant" size={50} color="white" />
+                    </TouchableOpacity>
+
+                    <TouchableOpacity
+                        onPress={async() => {
+                            if (cameraRef) {
+                                let photo = await cameraRef.takePictureAsync();
+                                setImage(photo.uri);
+                                setPhotoTaken(true);
+                                console.log('photo taken', photo);
                             }
-                            setImage(photo.uri);
-                        }
-                    }}>
-                    <MaterialCommunityIcons name="circle-slice-8" size={70} color="white" />
-                </TouchableOpacity>
-                <TouchableOpacity
-                    // For now, I have changed the camera roll icon to toggle the ghost image to remove for camera preview.
-                    onPress={() => {
-                        setGhostImage(prevCheck => !prevCheck);
-                        console.log('show ghost image =', ghostImage)
-                    }}>
-                    <MaterialIcons name="filter" size={50} color="white" />
-                </TouchableOpacity>
+                        }}>
+                        <MaterialCommunityIcons name="circle-slice-8" size={70} color="white" />
+                    </TouchableOpacity>
+                    <TouchableOpacity
+                        onPress={() => {
+                            setGhostImage(prevCheck => !prevCheck);
+                            console.log('show ghost image =', ghostImage)
+                        }}>
+                        <MaterialIcons name="filter" size={50} color="white" />
+                    </TouchableOpacity>
+                </View>
             </View>
-            <Review picture={image}/>
         </View>
+    );
+}
+
+const ViewTopBar = () => {
+    return (
+        <SafeAreaView style={styles.topRow}>
+            <TouchableOpacity onPress={() => Alert.alert('Arrow Pressed')}>
+                <AntDesign name="arrowleft" size={35} color="black"/>
+            </TouchableOpacity>
+            <TouchableOpacity onPress={() => Alert.alert('Help Pressed')}>
+                <AntDesign name="questioncircleo" size={35} color="black" />
+            </TouchableOpacity>
+        </SafeAreaView>
     );
 }
 
@@ -134,7 +145,7 @@ const styles = StyleSheet.create({
         height: undefined,
         aspectRatio: 1.5,
         resizeMode: 'contain'
-    }
+    },
 });
 
 export default CameraScreen;
