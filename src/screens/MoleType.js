a7bd4e22070a2aa04176b4020adbb1dc5ebc2ffd
ambika_a <57152715+Ambikaa1@users.noreply.github.com>
2021-03-01 22:00:59 +0000

    changed button

--- src/screens/MoleType.js
+++ src/screens/MoleType.js
@@ -2,7 +2,6 @@ import React from 'react';
 import {StyleSheet, View, Text, TouchableOpacity, Image} from 'react-native';
 import DropDownPicker from 'react-native-dropdown-picker';
 
-
 const MoleType = ({navigation}) => {
     return (
         <View style={{flex: 1, flexDirection: 'column', justifyContent: 'center', alignItems: 'center'}}>
@@ -19,11 +18,13 @@ const MoleType = ({navigation}) => {
                 />
             </View>
             <View style={{flex: 6, justifyContent: 'center', width:250}}>
-                <Text> Space to add images if an existing mole </Text>
+                <Text> Space to add images if an existing mole.</Text>
+                <Text> If it is we need to ask them for info about it e.g. mole name </Text>
+                <Text> Can be done after we have diary screen done as page is similar </Text>
             </View>
             <View style={{flex: 2.5, width: 200, justifyContent: 'center'}}>
                 <TouchableOpacity style={styles.button} onPress={() => navigation.navigate("CameraFar")}>
-                    <Text>Confirm choice</Text>
+                    <Text style = {styles.text}> Confirm choice</Text>
                  </TouchableOpacity>
             </View>
         </View>
@@ -37,7 +38,10 @@ const styles = StyleSheet.create({
     button: {
         alignItems: "center",
         backgroundColor: "#71A1D1",
-        padding: 15
+        padding: 15,
+    },
+    text: {
+        color: "white",
     },
 });
 
