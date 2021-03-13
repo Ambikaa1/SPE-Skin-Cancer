6d02611bc1dd2d0e2552f11257e7fe287a9bc811
ambika_a <57152715+Ambikaa1@users.noreply.github.com>
2021-03-13 13:52:55 +0000

    Left homunc done

--- src/screens/LeftHomuncScreen.js
+++ src/screens/LeftHomuncScreen.js
@@ -11,35 +11,19 @@ const LeftHomuncScreen = ({navigation}) => {
                 <TouchableOpacity onPress={() => navigation.navigate("BackHomunc")}>
                     <Image style={styles.rotate} source={require('../../assets/Side/BackView.png')} />
                 </TouchableOpacity>
-                <TouchableOpacity onPress={() => clickHandler("Head or Neck")}>
-                    <Image style={styles.head} source={require('../../assets/Front/Head.png')} />
+                <TouchableOpacity onPress={() => clickHandler("Left Head or Neck")}>
+                    <Image style={styles.head} source={require('../../assets/Side/LeftHead.png')} />
                 </TouchableOpacity>
                 <TouchableOpacity onPress={() => navigation.navigate("FrontHomunc")}>
                     <Image style={styles.rotate} source={require('../../assets/Side/FrontView.png')} />
                 </TouchableOpacity>
             </View>
-            <View style={styles.flexbox}>
-                <TouchableOpacity onPress={() => clickHandler("Right Upper Arm")}>
-                    <Image style={styles.arm} source={require('../../assets/Front/RightUA.png')} />
-                </TouchableOpacity>
-                <TouchableOpacity onPress={() => clickHandler("Torso")}>
-                    <Image style={styles.chest} source={require('../../assets/Front/ChestTop.png')} />
-                </TouchableOpacity>
-                <TouchableOpacity onPress={() => clickHandler("Left Upper Arm")}>
-                    <Image style={styles.arm} source={require('../../assets/Front/LeftUA.png')} />
-                </TouchableOpacity>
-            </View>
-            <View style={styles.flexbox}>
-                <TouchableOpacity onPress={() => clickHandler("Right Lower Arm")}>
-                    <Image style={styles.lowerArm} source={require('../../assets/Front/RightLA.png')} />
-                </TouchableOpacity>
-                <TouchableOpacity onPress={() => clickHandler("Torso")}>
-                    <Image style={styles.body} source={require('../../assets/Front/FrontMiddle.png')} />
-                </TouchableOpacity>
-                <TouchableOpacity onPress={() => clickHandler("Left Lower Arm")}>
-                    <Image style={styles.lowerArm} source={require('../../assets/Front/LeftLA.png')} />
-                </TouchableOpacity>
-            </View>
+            <TouchableOpacity onPress={() => clickHandler("Left Torso")}>
+                <Image style={styles.torso} source={require('../../assets/Side/LeftTorso.png')} />
+            </TouchableOpacity>
+            <TouchableOpacity onPress={() => clickHandler("Left Legs")}>
+                <Image style={styles.legs} source={require('../../assets/Side/LeftLegs.png')} />
+            </TouchableOpacity>
         </ScrollView>
     );
 };
@@ -54,54 +38,25 @@ const styles = StyleSheet.create({
         flexDirection: 'row',
         justifyContent: 'center',
     },
-    // head: {
-    //     width: 115,
-    //     height: 95,
-    //     resizeMode: 'contain',
-    // },
-    // arm: {
-    //     width: 75,
-    //     height: 100,
-    //     resizeMode: 'contain',
-    // },
-    // chest: {
-    //     width: 110,
-    //     height: 100,
-    //     resizeMode: 'contain',
-    // },
-    // lowerArm: {
-    //     width: 76,
-    //     height: 58,
-    //     resizeMode: 'contain',
-    // },
-    // body: {
-    //     width: 109,
-    //     height: 57,
-    //     resizeMode: 'contain',
-    // },
-    // hand: {
-    //     width: 69,
-    //     height: 55,
-    //     resizeMode: 'contain',
-    // },
-    // leg: {
-    //     width: 137,
-    //     height: 87,
-    //     resizeMode: 'contain',
-    // },
-    // lowerLeg: {
-    //     width: 133,
-    //     height: 90,
-    //     resizeMode: 'contain',
-    // },
-    // foot: {
-    //     width: 138,
-    //     height: 50,
-    //     resizeMode: 'contain',
-    // },
+    head: {
+        width: 140,
+        height: 80,
+        resizeMode: 'contain',
+    },
+    torso: {
+        width: 360,
+        height: 175,
+        resizeMode: 'contain',
+    },
+    legs: {
+        width: 370,
+        height: 190,
+        resizeMode: 'contain',
+        alignSelf: 'center',
+    },
     rotate: {
-        width: 160,
-        height: 95,
+        width: 150,
+        height: 80,
         resizeMode: 'contain',
     },
 });
