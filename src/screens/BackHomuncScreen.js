0aff94550c57b11991e4ae028670ea1983a11429
ambika_a <57152715+Ambikaa1@users.noreply.github.com>
2021-03-17 16:27:59 +0000

    added all back photos

--- src/screens/BackHomuncScreen.js
+++ src/screens/BackHomuncScreen.js
@@ -12,69 +12,64 @@ const BackHomuncScreen = ({navigation}) => {
                     <Image style={styles.rotate} source={require('../../assets/Back/LeftView.png')} />
                 </TouchableOpacity>
                 <TouchableOpacity onPress={() => clickHandler("Head or Neck")}>
-                    <Image style={styles.head} source={require('../../assets/Front/Head.png')} />
+                    <Image style={styles.head} source={require('../../assets/Back/Head.png')} />
                 </TouchableOpacity>
                 <TouchableOpacity onPress={() => navigation.navigate("RightHomunc")}>
                     <Image style={styles.rotate} source={require('../../assets/Back/RightView.png')} />
                 </TouchableOpacity>
             </View>
-            {/*<View style={styles.flexbox}>*/}
-            {/*    <TouchableOpacity onPress={() => clickHandler("Right Upper Arm")}>*/}
-            {/*        <Image style={styles.arm} source={require('../../assets/Front/RightUA.png')} />*/}
-            {/*    </TouchableOpacity>*/}
-            {/*    <TouchableOpacity onPress={() => clickHandler("Torso")}>*/}
-            {/*        <Image style={styles.chest} source={require('../../assets/Front/ChestTop.png')} />*/}
-            {/*    </TouchableOpacity>*/}
-            {/*    <TouchableOpacity onPress={() => clickHandler("Left Upper Arm")}>*/}
-            {/*        <Image style={styles.arm} source={require('../../assets/Front/LeftUA.png')} />*/}
-            {/*    </TouchableOpacity>*/}
-            {/*</View>*/}
-            {/*<View style={styles.flexbox}>*/}
-            {/*    <TouchableOpacity onPress={() => clickHandler("Right Lower Arm")}>*/}
-            {/*        <Image style={styles.lowerArm} source={require('../../assets/Front/RightLA.png')} />*/}
-            {/*    </TouchableOpacity>*/}
-            {/*    <TouchableOpacity onPress={() => clickHandler("Torso")}>*/}
-            {/*        <Image style={styles.body} source={require('../../assets/Front/FrontMiddle.png')} />*/}
-            {/*    </TouchableOpacity>*/}
-            {/*    <TouchableOpacity onPress={() => clickHandler("Left Lower Arm")}>*/}
-            {/*        <Image style={styles.lowerArm} source={require('../../assets/Front/LeftLA.png')} />*/}
-            {/*    </TouchableOpacity>*/}
-            {/*</View>*/}
-            {/*<View style={styles.flexbox}>*/}
-            {/*    <TouchableOpacity onPress={() => clickHandler("Volar Right Hand")}>*/}
-            {/*        <Image style={styles.hand} source={require('../../assets/Front/RightHand.png')} />*/}
-            {/*    </TouchableOpacity>*/}
-            {/*    <TouchableOpacity onPress={() => clickHandler("Torso")}>*/}
-            {/*        <Image style={styles.body} source={require('../../assets/Front/FrontLower.png')} />*/}
-            {/*    </TouchableOpacity>*/}
-            {/*    <TouchableOpacity onPress={() => clickHandler("Volar Left Hand")}>*/}
-            {/*        <Image style={styles.hand} source={require('../../assets/Front/LeftHand.png')} />*/}
-            {/*    </TouchableOpacity>*/}
-            {/*</View>*/}
-            {/*<View style = {styles.flexbox}>*/}
-            {/*    <TouchableOpacity onPress={() => clickHandler("Right Upper Leg")}>*/}
-            {/*        <Image style={styles.leg} source={require('../../assets/Front/RightUL.png')} />*/}
-            {/*    </TouchableOpacity>*/}
-            {/*    <TouchableOpacity onPress={() => clickHandler("Left Upper Leg")}>*/}
-            {/*        <Image style={styles.leg} source={require('../../assets/Front/LeftUL.png')} />*/}
-            {/*    </TouchableOpacity>*/}
-            {/*</View>*/}
-            {/*<View style={styles.flexbox}>*/}
-            {/*    <TouchableOpacity onPress={() => clickHandler("Right Lower Leg")}>*/}
-            {/*        <Image style={styles.lowerLeg} source={require('../../assets/Front/RightLL.png')} />*/}
-            {/*    </TouchableOpacity>*/}
-            {/*    <TouchableOpacity onPress={() => clickHandler("Left Lower Leg")}>*/}
-            {/*        <Image style={styles.lowerLeg} source={require('../../assets/Front/LeftLL.png')} />*/}
-            {/*    </TouchableOpacity>*/}
-            {/*</View>*/}
-            {/*<View style={styles.flexbox}>*/}
-            {/*    <TouchableOpacity onPress={() => clickHandler("Dorsum Right Foot")}>*/}
-            {/*        <Image style={styles.foot} source={require('../../assets/Front/RightFoot.png')} />*/}
-            {/*    </TouchableOpacity>*/}
-            {/*    <TouchableOpacity onPress={() => clickHandler("Dorsum Left Foot")}>*/}
-            {/*        <Image style={styles.foot} source={require('../../assets/Front/LeftFoot.png')} />*/}
-            {/*    </TouchableOpacity>*/}
-            {/*</View>*/}
+            <View style={styles.flexbox}>
+                <TouchableOpacity onPress={() => clickHandler("Right Upper Arm")}>
+                    <Image style={styles.arm} source={require('../../assets/Back/LeftUA.png')} />
+                </TouchableOpacity>
+                <TouchableOpacity onPress={() => clickHandler("Torso")}>
+                    <Image style={styles.chest} source={require('../../assets/Back/BackTorso.png')} />
+                </TouchableOpacity>
+                <TouchableOpacity onPress={() => clickHandler("Left Upper Arm")}>
+                    <Image style={styles.arm} source={require('../../assets/Back/RightUA.png')} />
+                </TouchableOpacity>
+            </View>
+            <View style={styles.flexbox}>
+                <TouchableOpacity onPress={() => clickHandler("Right Lower Arm")}>
+                    <Image style={styles.lowerArm} source={require('../../assets/Back/LeftLA.png')} />
+                </TouchableOpacity>
+                <TouchableOpacity onPress={() => clickHandler("Torso")}>
+                    <Image style={styles.body} source={require('../../assets/Back/LowerTorso.png')} />
+                </TouchableOpacity>
+                <TouchableOpacity onPress={() => clickHandler("Left Lower Arm")}>
+                    <Image style={styles.lowerArm} source={require('../../assets/Back/RightLA.png')} />
+                </TouchableOpacity>
+            </View>
+            <View style={styles.flexbox}>
+                <TouchableOpacity onPress={() => clickHandler("Volar Right Hand")}>
+                    <Image style={styles.hand} source={require('../../assets/Back/LeftHand.png')} />
+                </TouchableOpacity>
+                <TouchableOpacity onPress={() => clickHandler("Torso")}>
+                    <Image style={styles.leg} source={require('../../assets/Back/LeftUL.png')} />
+                </TouchableOpacity>
+                <TouchableOpacity onPress={() => clickHandler("Torso")}>
+                    <Image style={styles.leg} source={require('../../assets/Back/RightUL.png')} />
+                </TouchableOpacity>
+                <TouchableOpacity onPress={() => clickHandler("Volar Left Hand")}>
+                    <Image style={styles.hand} source={require('../../assets/Back/RightHand.png')} />
+                </TouchableOpacity>
+            </View>
+            <View style={styles.flexbox}>
+                <TouchableOpacity onPress={() => clickHandler("Right Lower Leg")}>
+                    <Image style={styles.lowerLeg} source={require('../../assets/Back/LeftLL.png')} />
+                </TouchableOpacity>
+                <TouchableOpacity onPress={() => clickHandler("Left Lower Leg")}>
+                    <Image style={styles.lowerLeg} source={require('../../assets/Back/RightLL.png')} />
+                </TouchableOpacity>
+            </View>
+            <View style={styles.flexbox}>
+                <TouchableOpacity onPress={() => clickHandler("Dorsum Right Foot")}>
+                    <Image style={styles.foot} source={require('../../assets/Back/LeftFoot.png')} />
+                </TouchableOpacity>
+                <TouchableOpacity onPress={() => clickHandler("Dorsum Left Foot")}>
+                    <Image style={styles.foot} source={require('../../assets/Back/RightFoot.png')} />
+                </TouchableOpacity>
+            </View>
         </ScrollView>
     );
 };
@@ -89,51 +84,51 @@ const styles = StyleSheet.create({
         flexDirection: 'row',
         justifyContent: 'center',
     },
-    // head: {
-    //     width: 115,
-    //     height: 95,
-    //     resizeMode: 'contain',
-    // },
-    // arm: {
-    //     width: 75,
-    //     height: 100,
-    //     resizeMode: 'contain',
-    // },
-    // chest: {
-    //     width: 110,
-    //     height: 100,
-    //     resizeMode: 'contain',
-    // },
-    // lowerArm: {
-    //     width: 76,
-    //     height: 58,
-    //     resizeMode: 'contain',
-    // },
-    // body: {
-    //     width: 109,
-    //     height: 57,
-    //     resizeMode: 'contain',
-    // },
-    // hand: {
-    //     width: 69,
-    //     height: 55,
-    //     resizeMode: 'contain',
-    // },
-    // leg: {
-    //     width: 137,
-    //     height: 87,
-    //     resizeMode: 'contain',
-    // },
-    // lowerLeg: {
-    //     width: 133,
-    //     height: 90,
-    //     resizeMode: 'contain',
-    // },
-    // foot: {
-    //     width: 138,
-    //     height: 50,
-    //     resizeMode: 'contain',
-    // },
+    head: {
+        width: 115,
+        height: 95,
+        resizeMode: 'contain',
+    },
+    arm: {
+        width: 75,
+        height: 100,
+        resizeMode: 'contain',
+    },
+    chest: {
+        width: 110,
+        height: 100,
+        resizeMode: 'contain',
+    },
+    lowerArm: {
+        width: 76,
+        height: 58,
+        resizeMode: 'contain',
+    },
+    body: {
+        width: 109,
+        height: 57,
+        resizeMode: 'contain',
+    },
+    hand: {
+        width: 69,
+        height: 55,
+        resizeMode: 'contain',
+    },
+    leg: {
+        width: 137,
+        height: 87,
+        resizeMode: 'contain',
+    },
+    lowerLeg: {
+        width: 133,
+        height: 90,
+        resizeMode: 'contain',
+    },
+    foot: {
+        width: 138,
+        height: 50,
+        resizeMode: 'contain',
+    },
     rotate: {
         width: 160,
         height: 95,
