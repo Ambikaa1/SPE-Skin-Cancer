ed7fe959752edb8c923c2d5da432785c24b56ff3
ambika_a <57152715+Ambikaa1@users.noreply.github.com>
2021-03-19 16:22:47 +0000

    back homunc test

---
+++ src/screens/__tests__/MoleTypeScreen.test.js
@@ -0,0 +1,11 @@
+// import React from 'react';
+// import renderer from 'react-test-renderer';
+//
+// import MoleTypeScreen from "../MoleTypeScreen";
+//
+// describe('<MoleTypeScreen />', () => {
+//     it('renders correctly', () => {
+//         const tree = renderer.create(<MoleTypeScreen />).toJSON();
+//         expect(tree).toMatchSnapshot();
+//     });
+// });
