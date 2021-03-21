4b894505fb86505a1c0b4a02e6ab390c57772450
George Herbert <george.herbert@icloud.com>
2021-03-21 12:44:57 +0000

    Homepage now shows next five moles that need to be photographed

--- src/components/MoleCountdown.js
+++ src/components/MoleCountdown.js
@@ -2,10 +2,25 @@ import React from "react";
 import { View, Text, StyleSheet } from "react-native";
 
 const MoleCountdown = ({ item }) => {
+    const percentage = (item.nextUpdate - Date.now()) / 2592000000;
+
+    const barStyle = () => {
+        console.log(percentage);
+        return ({
+            marginTop: 5,
+            height: 10,
+            width: `${percentage * 10}%`,
+            backgroundColor: "#71A1D1"
+        })
+    }
+
     return (
         <View style = {styles.container}>
             <Text style = {styles.name}>{item.name}</Text>
-            <View style = {styles.bar} />
+            <View style = {styles.bar}>
+                <View style = {barStyle()} />
+                <View style = {styles.remainingBar} />
+            </View>
         </View>
     );
 };
@@ -20,10 +35,14 @@ const styles = StyleSheet.create({
         fontSize: 17
     },
     bar: {
+        flexDirection: "row"
+    },
+    remainingBar: {
         marginTop: 5,
         height: 10,
-        // width: 10,
-        backgroundColor: "#71A1D1"
+        backgroundColor: "#71A1D120",
+        opacity: 10,
+        flex: 1,
     }
 });
 
