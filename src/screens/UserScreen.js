7e419d8edc24448aa055f9e132eb01c4108b04ff
ambika_a <57152715+Ambikaa1@users.noreply.github.com>
2021-03-24 23:01:58 +0000

    forgot to update snapshot

--- src/screens/UserScreen.js
+++ src/screens/UserScreen.js
@@ -40,7 +40,6 @@ const UserScreen = ({navigation}) => {
     <View style = {styles.container}>
       <Text style = {styles.info}>You now need to put in some basic information to help the App identify you and your moles.</Text>
       <Text style = {styles.info}>This information remains on your phone unless you want to send it with your images by email to a clinician such as your GP or skin doctor.</Text>
-      {/*<Text style = {styles.title}> Basic Information</Text>*/}
       <TextInput
         value = {firstName}
         onChangeText = {value => setFirstName(value)}
