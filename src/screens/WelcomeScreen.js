19d0f68cf13929862c882619b9311e1e5bd5f0d9
SHold <ye1986@bristol.ac.uk>
2021-03-26 14:56:16 +0000

    Fixed flow of the near shot - far shot

---
+++ src/screens/WelcomeScreen.js
@@ -0,0 +1,67 @@
+
+import React from "react";
+import {SafeAreaView, Text, StyleSheet,TouchableOpacity} from "react-native";
+
+
+const WelcomeScreen = ({ navigation }) => {
+
+
+    return (
+        <SafeAreaView style = {styles.container}>
+            <Text style = {styles.titleText}>WELCOME!</Text>
+
+            <Text style = {styles.mainText} >
+                This App is designed to allow you to monitor your skin moles and similar lesions.
+                Lesions are areas of abnormal change, for example a wound, an ulcer or a skin tumour.
+                It was developed through a collaboration between The Skin Cancer Research Fund (SCaRF)
+                and The Computer Science Department of the University of Bristol.
+            </Text>
+
+            <TouchableOpacity style = {styles.doneBox} onPress = {() => navigation.navigate("HomeScreen")}>
+                <Text style = {styles.doneText}>Done</Text>
+            </TouchableOpacity>
+
+
+        </SafeAreaView>
+    );
+};
+
+
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1
+    },
+    titleText: {
+        marginTop: 10,
+        marginLeft: 10,
+        fontSize: 25,
+        fontWeight: "bold"
+    },
+    mainText: {
+        marginTop: 10,
+        marginLeft: 10,
+        fontSize: 17,
+
+    },
+    doneBox: {
+        marginHorizontal: 10,
+        marginTop: 20,
+        backgroundColor: "#71A1D1",
+        alignItems: "center",
+        borderRadius: 10,
+        width: "95%",
+        bottom: 10,
+        zIndex: 1,
+    },
+    doneText: {
+        fontSize: 30,
+        fontWeight: "bold",
+        color: "white",
+        marginVertical: 10,
+        zIndex: 1,
+    },
+});
+
+export default WelcomeScreen;
+
