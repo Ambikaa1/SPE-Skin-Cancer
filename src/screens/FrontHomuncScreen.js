52373bf9e68d07f69ee8e06266d696f453035185
ambika_a <57152715+Ambikaa1@users.noreply.github.com>
2021-03-26 17:53:59 +0000

    navigation was wrong

--- src/screens/FrontHomuncScreen.js
+++ src/screens/FrontHomuncScreen.js
@@ -10,13 +10,13 @@ const FrontHomuncScreen = ({navigation}) => {
             <Text style = {styles.title}>Please click on the body part where your mole is located. </Text>
             <Text style = {styles.title}>Swipe left or right, or click on the buttons to navigate to the different views. </Text>
             <View style={styles.flexbox}>
-                <TouchableOpacity onPress={() => navigation.navigate("RightHomunc")}>
+                <TouchableOpacity onPress={() => navigation.navigate("LeftHomunc")}>
                     <Image style={styles.rotate} source={require('../../assets/Back/Left3.png')} />
                 </TouchableOpacity>
                 <TouchableOpacity onPress={() => navigation.navigate("BackHomunc")}>
                     <Image style={styles.rotate} source={require('../../assets/Back/Back3.png')} />
                 </TouchableOpacity>
-                <TouchableOpacity onPress={() => navigation.navigate("LeftHomunc")}>
+                <TouchableOpacity onPress={() => navigation.navigate("RightHomunc")}>
                     <Image style={styles.rotate} source={require('../../assets/Back/Right3.png')} />
                 </TouchableOpacity>
             </View>
