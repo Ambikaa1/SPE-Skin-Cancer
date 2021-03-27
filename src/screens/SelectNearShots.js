ad4a3f972a173c2a8e5b9ecc37313e9e4916a1a8
SHold <ye1986@bristol.ac.uk>
2021-03-27 02:52:46 +0000

    Completed functionality of: selecting and unselecting photos then adding uris to an array

--- src/screens/SelectNearShots.js
+++ src/screens/SelectNearShots.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
-import { View, Text, StyleSheet, FlatList, Image } from "react-native";
+import { View, Text, StyleSheet, FlatList, TouchableOpacity, Image } from "react-native";
 import * as SQLite from "expo-sqlite";
+
 
 const db = SQLite.openDatabase("22.db");
 
@@ -20,23 +21,41 @@ const SelectNearShots = ({ route }) => {
         );
     }, []);
 
-    const displayImages = ({ item }) => {
-        return (
-            <View style={styles.nearFarShot}>
+    let selectedImages = [];
+
+    const displayFarShots = ({ item }) => {
+        return(
+            <View style = {styles.nearFarShot}>
+            <TouchableOpacity style = {styles.nearFarShot} onPress = {() => HandleMultipleSelection(item.near_shot)}>
                 <Image
                     style = {styles.image}
                     source = {{ uri: item.near_shot }}
                 />
-                <Text style={styles.moleDetails}>Date taken: {item.date}</Text>
+            </TouchableOpacity>
+                <View style = {styles.moleInfo}>
+                    <Text style = {styles.moleName}>{item.name}</Text>
+                    <Text style = {styles.moleDetails}>{item.comments}</Text>
+                    <Text style = {styles.moleDetails}>Last updated: {item.date}</Text>
+                </View>
             </View>
         );
+    };
+
+    const HandleMultipleSelection = (uri) => {
+        if (selectedImages.includes(uri))
+            selectedImages =  selectedImages.filter(_uri => _uri !== uri)
+        else
+            selectedImages.push(uri)
+        console.log(selectedImages)
     }
+
     return (
         <View style={styles.container}>
             <Text style = {styles.title}>Near shot images:</Text>
             <FlatList
                 data = {entries}
-                renderItem = {displayImages}
+                extraData = {selectedImages}
+                renderItem = {displayFarShots}
                 keyExtractor = {() => `${Math.floor(Math.random() * 10000)}`}
             />
         </View>
@@ -56,6 +75,7 @@ const styles = StyleSheet.create({
         marginHorizontal: 10,
         paddingVertical: 5,
         marginVertical: 5,
+        borderStyle: 'solid',
     },
     title: {
         marginTop: 10,
