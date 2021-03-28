5c5ed0c53d4a4d447026e0da5228d423ac91b15d
George Herbert <george.herbert@icloud.com>
2021-03-28 15:19:59 +0100

    Renamed file

--- src/screens/DiaryScreen.js
+++ src/screens/DiaryScreen.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { Text, StyleSheet, TouchableOpacity, FlatList } from "react-native";
 import { useIsFocused } from "@react-navigation/native";
-import DisplayImages from "../components/MoleList"
+import DisplayImages from "../components/MoleListItem"
 import * as SQLite from "expo-sqlite";
 
 const db = SQLite.openDatabase("22.db");
