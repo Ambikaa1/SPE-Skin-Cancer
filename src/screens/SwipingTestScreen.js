cc10acedfe1abb4294e543502a85a478b82d269c
ambika_a <57152715+Ambikaa1@users.noreply.github.com>
2021-03-31 22:51:02 +0100

    slightly better but unfinished version

--- src/screens/SwipingTestScreen.js
+++ src/screens/SwipingTestScreen.js
@@ -1,8 +1,11 @@
 import React from 'react'
-import {Image, StyleSheet, Text, TouchableOpacity, View} from 'react-native'
+import {Image, ScrollView, StyleSheet, Text, TouchableOpacity, View} from 'react-native'
 import Swiper from 'react-native-swiper'
 
 const SwipingTestScreen = ({navigation}) => {
+    const clickHandler = (bodyPart) => {
+        navigation.navigate("SideBodyPart", {bodyPart: bodyPart})
+    }
     return (
         <Swiper style={styles.wrapper} showsButtons loop={false}>
             <View style={styles.slide1}>
@@ -10,21 +13,66 @@ const SwipingTestScreen = ({navigation}) => {
                     <Image source={require('../../assets/Back/Front3.png')} />
                 </TouchableOpacity>
             </View>
-            <View style={styles.slide1}>
-                <TouchableOpacity onPress={() => navigation.navigate("LeftHomunc")}>
-                    <Image source={require('../../assets/Back/Left3.png')} />
-                </TouchableOpacity>
-            </View>
-            <View style={styles.slide1}>
-                <TouchableOpacity onPress={() => navigation.navigate("BackHomunc")}>
-                    <Image source={require('../../assets/Back/Back3.png')} />
-                </TouchableOpacity>
-            </View>
-            <View style={styles.slide1}>
-                <TouchableOpacity onPress={() => navigation.navigate("RightHomunc")}>
-                    <Image source={require('../../assets/Back/Right3.png')} />
-                </TouchableOpacity>
-            </View>
+            <ScrollView persistentScrollbar={true} style={styles.scrollView}>
+                <Text style = {styles.title}>Please scroll down to click on the body part where your mole is located. </Text>
+                <Text style = {styles.title}>Swipe left or right, or click on the buttons to navigate to the different views. </Text>
+                <View style={styles.flexbox}>
+                    <TouchableOpacity onPress={() => navigation.navigate("LeftHomunc")}>
+                        <Image style={styles.rotate} source={require('../../assets/Back/Left3.png')} />
+                    </TouchableOpacity>
+                    <TouchableOpacity onPress={() => navigation.navigate("FrontHomunc")}>
+                        <Image style={styles.rotate} source={require('../../assets/Back/Front3.png')} />
+                    </TouchableOpacity>
+                    <TouchableOpacity onPress={() => navigation.navigate("BackHomunc")}>
+                        <Image style={styles.rotate} source={require('../../assets/Back/Back3.png')} />
+                    </TouchableOpacity>
+                </View>
+                <View style={styles.flexbox}>
+                    <TouchableOpacity onPress={() => clickHandler("Right Head or Neck")}>
+                        <Image style={styles.head} source={require('../../assets/Side/RHead.png')} />
+                    </TouchableOpacity>
+                </View>
+                <View style={styles.flexbox}>
+                    <TouchableOpacity onPress={() => clickHandler("Right Torso")}>
+                        <Image style={styles.torso} source={require('../../assets/Side/RTorso.png')} />
+                    </TouchableOpacity>
+                </View>
+                <View style={styles.flexbox}>
+                    <TouchableOpacity onPress={() => clickHandler("Right Legs")}>
+                        <Image style={styles.legs} source={require('../../assets/Side/RLegs.png')} />
+                    </TouchableOpacity>
+                </View>
+            </ScrollView>
+            <ScrollView persistentScrollbar={true} style={styles.scrollView}>
+                <Text style = {styles.title}>Please scroll down to click on the body part where your mole is located. </Text>
+                <Text style = {styles.title}>Swipe left or right, or click on the buttons to navigate to the different views. </Text>
+                <View style={styles.flexbox}>
+                    <TouchableOpacity onPress={() => navigation.navigate("BackHomunc")}>
+                        <Image style={styles.rotate} source={require('../../assets/Back/Back3.png')} />
+                    </TouchableOpacity>
+                    <TouchableOpacity onPress={() => navigation.navigate("FrontHomunc")}>
+                        <Image style={styles.rotate} source={require('../../assets/Back/Front3.png')} />
+                    </TouchableOpacity>
+                    <TouchableOpacity onPress={() => navigation.navigate("RightHomunc")}>
+                        <Image style={styles.rotate} source={require('../../assets/Back/Right3.png')} />
+                    </TouchableOpacity>
+                </View>
+                <View style={styles.flexbox}>
+                    <TouchableOpacity onPress={() => clickHandler("Left Head or Neck")}>
+                        <Image style={styles.head} source={require('../../assets/Side/LHead.png')} />
+                    </TouchableOpacity>
+                </View>
+                <View style={styles.flexbox}>
+                    <TouchableOpacity onPress={() => clickHandler("Left Torso")}>
+                        <Image style={styles.torso} source={require('../../assets/Side/LTorso.png')} />
+                    </TouchableOpacity>
+                </View>
+                <View style={styles.flexbox}>
+                    <TouchableOpacity onPress={() => clickHandler("Left Legs")}>
+                        <Image style={styles.legs} source={require('../../assets/Side/LLegs.png')} />
+                    </TouchableOpacity>
+                </View>
+            </ScrollView>
         </Swiper>
     )
 };
@@ -42,6 +90,37 @@ const styles = StyleSheet.create({
         justifyContent: 'center',
         alignItems: 'center',
         backgroundColor: '#9DD6EB'
+    },
+    scrollView: {
+        backgroundColor: 'white',
+    },
+    flexbox: {
+        flexDirection: 'row',
+        justifyContent: 'center',
+    },
+    head: {
+        width: 350,
+        height: 99,
+        resizeMode: 'contain',
+    },
+    torso: {
+        width: 300,
+        height: 220,
+        resizeMode: 'contain',
+    },
+    legs: {
+        width: 360,
+        height: 245,
+        resizeMode: 'contain',
+    },
+    rotate: {
+        width: 120,
+        height: 120,
+        resizeMode: 'contain',
+    },
+    title:{
+        fontSize: 15,
+        fontWeight: "bold",
     },
 });
 
