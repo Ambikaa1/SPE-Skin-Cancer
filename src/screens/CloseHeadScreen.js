fcbf3df7fe883f7c10ee7d485dfb72fb352a6672
George Herbert <george.herbert@icloud.com>
2021-04-12 15:46:40 +0100

    Unbolded for consistency but may be changed back

--- src/screens/CloseHeadScreen.js
+++ src/screens/CloseHeadScreen.js
@@ -63,7 +63,7 @@ const styles = StyleSheet.create({
     },
     title:{
         fontSize: 17,
-        fontWeight: "bold",
+        // fontWeight: "bold",
         marginTop: 10,
         marginHorizontal: 10,
     },
