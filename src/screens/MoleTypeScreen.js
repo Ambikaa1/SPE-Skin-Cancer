5084cbb70c3c4b41d9f5cf3d5fe4911ee5da7784
George Herbert <george.herbert@icloud.com>
2021-04-12 16:12:22 +0100

    Can now tap anywhere on screen when typing to dismiss keyboard

--- src/screens/MoleTypeScreen.js
+++ src/screens/MoleTypeScreen.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { StyleSheet, View, Text, TouchableOpacity, TextInput, FlatList } from 'react-native';
+import { StyleSheet, Keyboard, TouchableWithoutFeedback, View, Text, TouchableOpacity, TextInput, FlatList } from 'react-native';
 import DropDownPicker from 'react-native-dropdown-picker';
 import * as SQLite from "expo-sqlite";
 
@@ -22,53 +22,55 @@ const MoleTypeScreen = ({navigation, route}) => {
     }, []);
 
     return (
-        <View style = {styles.container}>
-            <Text style = {styles.questionTop}>Have you logged this mole on the app before?</Text>
-            <DropDownPicker
-                items = {[
-                    {label: 'Yes', value: 0},
-                    {label: 'No', value: 1},
-                ]}
-                containerStyle = {styles.dropDownContainer}
-                labelStyle = {styles.dropDownLabel}
-                onChangeItem = {item => setChoice(item.value)}
-            />
-            {(moleChoice == 1) && 
-                <>
-                    <Text style = {styles.question}>Name this mole:</Text>
-                    <TextInput
-                        value = {name}
-                        onChangeText = {value => setName(value)}
-                        placeholder = "e.g. Left cheek 1"
-                        style = {styles.input}
-                    />
-                    <Text style = {styles.question}>Any other comments:</Text>
-                    <TextInput
-                        value = {comments}
-                        onChangeText = {value => setComments(value)}
-                        placeholder = "e.g. located 7cm away from nose"
-                        style = {styles.input}
-                    />
-                    <TouchableOpacity style = {styles.doneBox} onPress = {() => navigation.navigate("HelpFarShot", { name: name, comments: comments, bodyPart: route.params.bodyPart })}>
-                        <Text style = {styles.doneText}>Confirm</Text>
-                    </TouchableOpacity>
-                </>
-            }
-            {(moleChoice == 0) && 
-                <>
-                    <Text style = {styles.question}>Select a mole below to photograph</Text>
-                    <FlatList 
-                        data = {moles}
-                        renderItem = {({item}) => (
-                            <TouchableOpacity onPress = {() => navigation.navigate("HelpNearShot", { id: item.mole_id })}>
-                                <MoleListItem uri = {item.far_shot} name = {item.name} comments = {item.comments} lastUpdated = {item.lastUpdated} />
-                            </TouchableOpacity>
-                        )}
-                        keyExtractor = {item => `${item.mole_id}`}
-                    />
-                </>
-            }
-        </View>
+        <TouchableWithoutFeedback onPress={Keyboard.dismiss} accessible={false}>
+            <View style = {styles.container}>
+                <Text style = {styles.questionTop}>Have you logged this mole on the app before?</Text>
+                <DropDownPicker
+                    items = {[
+                        {label: 'Yes', value: 0},
+                        {label: 'No', value: 1},
+                    ]}
+                    containerStyle = {styles.dropDownContainer}
+                    labelStyle = {styles.dropDownLabel}
+                    onChangeItem = {item => setChoice(item.value)}
+                />
+                {(moleChoice == 1) && 
+                    <>
+                        <Text style = {styles.question}>Name this mole:</Text>
+                        <TextInput
+                            value = {name}
+                            onChangeText = {value => setName(value)}
+                            placeholder = "e.g. Left cheek 1"
+                            style = {styles.input}
+                        />
+                        <Text style = {styles.question}>Any other comments:</Text>
+                        <TextInput
+                            value = {comments}
+                            onChangeText = {value => setComments(value)}
+                            placeholder = "e.g. located 7cm away from nose"
+                            style = {styles.input}
+                        />
+                        <TouchableOpacity style = {styles.doneBox} onPress = {() => navigation.navigate("HelpFarShot", { name: name, comments: comments, bodyPart: route.params.bodyPart })}>
+                            <Text style = {styles.doneText}>Confirm</Text>
+                        </TouchableOpacity>
+                    </>
+                }
+                {(moleChoice == 0) && 
+                    <>
+                        <Text style = {styles.question}>Select a mole below to photograph</Text>
+                        <FlatList 
+                            data = {moles}
+                            renderItem = {({item}) => (
+                                <TouchableOpacity onPress = {() => navigation.navigate("HelpNearShot", { id: item.mole_id })}>
+                                    <MoleListItem uri = {item.far_shot} name = {item.name} comments = {item.comments} lastUpdated = {item.lastUpdated} />
+                                </TouchableOpacity>
+                            )}
+                            keyExtractor = {item => `${item.mole_id}`}
+                        />
+                    </>
+                }
+            </View>
+        </TouchableWithoutFeedback>
     );
 };
 
