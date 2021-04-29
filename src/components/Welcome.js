86eedb3609bc1de86e3addb3f1eead44e260a5af
George Herbert <george.herbert@icloud.com>
2021-04-29 12:39:17 +0100

    Added section on data collection

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -17,7 +17,12 @@ const Welcome = ({ setWelcomeSeen }) => {
                 and the Department of Computer Science at the University of Bristol.
             </Text>
             <Text style = {styles.mainText}>
-                Get started by tapping the user icon in the top left of the home screen and entering your details.
+                All data collected by this application is stored locally on your device and we do not have access to it.
+                This data includes your name and photographs of your moles, and is necessary in order for the app to function properly.
+                To begin using the application, if you are okay with the collection of this data, please click 'Done' below.
+            </Text>
+            <Text style = {styles.mainText}>
+                Once you have clicked the 'Done' button, get started by tapping the user icon in the top left of the home screen and entering your details.
             </Text>
 
             <TouchableOpacity style = {styles.doneBox} onPress = {() => {db.transaction(
