fb3e1067f6268abf328d96d7db623335a3a93ff9
George Herbert <george.herbert@icloud.com>
2021-05-07 14:11:07 +0100

    Uncommented logbox and fixed test

--- src/components/__tests__/Review.test.js
+++ src/components/__tests__/Review.test.js
@@ -9,6 +9,13 @@ const comments = "Comment";
 const id = 1;
 const bodyPart = "Foot";
 
+const DATE_TO_USE = new Date('2016');
+const _Date = Date;
+global.Date = jest.fn(() => DATE_TO_USE);
+global.Date.UTC = _Date.UTC;
+global.Date.parse = _Date.parse;
+global.Date.now = _Date.now;
+
 describe('<Review /> Near', () => {
     let nextScreen = "helpNear";
     it('renders correctly', () => {
